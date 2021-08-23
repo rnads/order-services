e7c4c7221cad1d37917a75a6f39297750d2c7e7a
Renan <progrnads@gmail.com>
2021-08-23 20:03:39 -0300

    create migrations

---
+++ .history/database/migrations/2021_08_23_223707_create_order_product_table_20210823193920.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->foreignId('service_orders_id')->constrained();
+            $table->foreignId('service_products_id')->constrained();
+            $table->primary(['service_orders_id', 'service_products_id']);
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
